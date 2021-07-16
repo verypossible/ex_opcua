e5a99dc4f1fe8edaff9cba02af8c71c59c9e0c07
Dan Lindeman <lindemda@gmail.com>
2021-07-16 15:18:20 -0400

    Better implementation of ExtensionObject

--- lib/data_types/extension_object.ex
+++ lib/data_types/extension_object.ex
@@ -5,6 +5,7 @@ defmodule ExOpcua.DataTypes.ExtensionObject do
     https://reference.opcfoundation.org/v104/Core/docs/Part6/5.1.5/
   """
   import ExOpcua.DataTypes.BuiltInDataTypes.Macros
+  alias ExOpcua.DataTypes.BuiltInDataTypes.OpcString
   alias ExOpcua.DataTypes.{NodeId, ServerStatus}
 
   @data_types %{
@@ -15,19 +16,30 @@ defmodule ExOpcua.DataTypes.ExtensionObject do
   end
 
   def take(bin) do
-    with {node, <<1, rest::binary>>} <- NodeId.take(bin) do
-      <<size::int(32), data::binary-size(size), rest::binary>> = rest
-      data_type = Map.get(@data_types, node.identifier, :not_implemented)
+    bin
+    |> NodeId.take()
+    |> take_body()
+  end
 
-      value =
-        if data_type != :not_implemented do
+  # no body info present
+  defp take_body({_node_id, <<0, rest::binary>>}), do: {nil, rest}
+
+  defp take_body({node_id, <<1, size::int(32), data::binary-size(size), rest::binary>>}) do
+    value =
+      case Map.fetch(@data_types, node_id.identifier) do
+        {:ok, data_type} ->
           {value, _} = data_type.take(data)
           value
-        else
-          :not_implemented
-        end
 
-      {value, rest}
-    end
+        :error ->
+          "Data Type Not Implemented"
+      end
+
+    {value, rest}
+  end
+
+  # XML Body is not parsed, returned as a string to user
+  defp take_body({_node_id, <<2, rest::binary>>}) do
+    OpcString.take(rest)
   end
 end
